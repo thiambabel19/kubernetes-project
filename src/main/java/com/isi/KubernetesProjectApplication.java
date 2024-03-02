faa1b5258fae73c48b28814cb68d441e29b96ae1
Mohamed THIAM <mohamedthiam3338@gmail.com>
2024-03-02 00:35:00 +0000

    Last commit

--- src/main/java/com/isi/KubernetesProjectApplication.java
+++ src/main/java/com/isi/KubernetesProjectApplication.java
@@ -2,9 +2,17 @@ package com.isi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class KubernetesProjectApplication {
+
+	@GetMapping("/message")
+	public String displayMessage(){
+		return "Deploiement effectue avec succes ...";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(KubernetesProjectApplication.class, args);
